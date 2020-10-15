d04867e1211c3276ff72885784b17376041eed68
Vaishnav Anil <vaishnavanil7th@gmail.com>
2020-10-15 16:19:28 +0530

    Added observation tick callback

--- app/src/main/java/in/ac/vitbhopal/projects/callrecorder/helper/PhoneStateChangeListener.java
+++ app/src/main/java/in/ac/vitbhopal/projects/callrecorder/helper/PhoneStateChangeListener.java
@@ -7,17 +7,29 @@ import java.util.Set;
 
 public abstract class PhoneStateChangeListener implements Disposable {
     private final Set<Consumer<PhoneState>> listeners = new HashSet<>();
-
+    private final Set<Consumer<PhoneState>> tickingListener = new HashSet<>();
     public final void onStateChange(Consumer<PhoneState> listener) {
         listeners.add(listener);
+    }
+    public final void  onObservationTick(Consumer<PhoneState> tickListener) {
+        listeners.add(tickListener);
     }
 
     public final void removeStateChangeListener(Consumer<PhoneState> listener) {
         listeners.remove(listener);
     }
 
+    public final void removeObserverTickListener(Consumer<PhoneState> listener) {
+        listeners.remove(listener);
+    }
+
     public final void notifyStateChange(PhoneState newState) {
         for (Consumer<PhoneState> listener: listeners) {
+            listener.accept(newState);
+        }
+    }
+    public final void notifyTick(PhoneState newState) {
+        for (Consumer<PhoneState> listener: tickingListener) {
             listener.accept(newState);
         }
     }
